54a4b53da012d3eec71cd1fc09c8e23aa4c88c95
amanvishwakarma96 <80578610+amanvishwakarma96@users.noreply.github.com>
2026-08-10 16:15:59 +0530

    Route provider settings by screen family

--- lib/app/platform/platform_screen_router.dart
+++ lib/app/platform/platform_screen_router.dart
@@ -1,13 +1,24 @@
 import 'package:flutter/widgets.dart';
 import 'package:mindly/app/platform/screen_family.dart';
 import 'package:mindly/screens/desktop/home/desktop_home_screen.dart';
+import 'package:mindly/screens/desktop/settings/desktop_provider_settings_screen.dart';
 import 'package:mindly/screens/mobile/home/mobile_home_screen.dart';
+import 'package:mindly/screens/mobile/settings/mobile_provider_settings_screen.dart';
 import 'package:mindly/screens/web/home/web_home_screen.dart';
+import 'package:mindly/screens/web/settings/web_provider_settings_screen.dart';
 
 Widget buildPlatformHome(ScreenFamily family) {
   return switch (family) {
     ScreenFamily.mobile => const MobileHomeScreen(),
     ScreenFamily.desktop => const DesktopHomeScreen(),
     ScreenFamily.web => const WebHomeScreen(),
+  };
+}
+
+Widget buildPlatformProviderSettings(ScreenFamily family) {
+  return switch (family) {
+    ScreenFamily.mobile => const MobileProviderSettingsScreen(),
+    ScreenFamily.desktop => const DesktopProviderSettingsScreen(),
+    ScreenFamily.web => const WebProviderSettingsScreen(),
   };
 }
