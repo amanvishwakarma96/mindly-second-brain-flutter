01f4fc7bb459b37712261c47a971a74214470db8
amanvishwakarma96 <80578610+amanvishwakarma96@users.noreply.github.com>
2026-08-10 18:46:59 +0530

    Pin Android NDK for native Whisper

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
 android {
     namespace = "com.amanvishwakarma96.mindly"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = flutter.ndkVersion
+    ndkVersion = "29.0.13113456"
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
